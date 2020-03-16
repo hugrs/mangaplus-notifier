3abf029dbe2a790093ba4be51e6d153439038d6f
Hugo Ros <hugo.ros@protonmail.com>
2020-03-16 17:43:32 +0200

    Make title info a class

--- fetchchapters.py
+++ fetchchapters.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import http.client
-import re
 import sys
 import os
 import json
@@ -14,41 +13,45 @@ gi.require_version("GLib","2.0")
 from gi.repository import Gtk, Gio, GLib
 
 DATA_DIR = os.path.join(Path.home(), ".mangaplus")
-BLOB_FILE = os.path.join(DATA_DIR, "response-blob")
+BLOB_FILE = os.path.join(DATA_DIR, "response-cache")
 META_FILE = os.path.join(DATA_DIR, "metadata")
+
+class Title:
+    def __init__(self, response_proto):
+        response = title_detail_pb2.Response()
+        response.ParseFromString(response_proto)
+        if response.HasField("error"):
+            print(response.error)
+            sys.exit(1)
+        self.detail = response.success.titleDetailView
+        
+    def get_latest_chapter(self):
+        if len(self.detail.lastChapterList) > 0:
+            chapter_list = self.detail.lastChapterList
+        else:
+            chapter_list = self.detail.firstChapterList
+        latest_chapter = chapter_list[-1]
+        latest_chapter_id = max([c.chapterId for c in chapter_list])
+        if latest_chapter.chapterId != latest_chapter_id:
+            print("Warning: last entry in chapter list is not the latest chapter")
+            print("Consider fixing the script to retrieve the chapter differently")
+        return latest_chapter
+    
+    def get_next_release_date(self):
+        return datetime.fromtimestamp(self.detail.nextTimestamp)
 
 def fetch_and_save_title_info():
     conn = http.client.HTTPSConnection("jumpg-webapi.tokyo-cdn.com")
     print("Making GET request to /api/title_detail")
     conn.request("GET", "/api/title_detail?title_id=100056")
 
-    res = conn.getresponse()
+    proto_blob = conn.getresponse().read()
     print("Request complete, saving response into file...")
-    with open(BLOB_FILE, "wb") as blob:
-        blob.write(res.read())
+    with open(BLOB_FILE, "wb") as file:
+        file.write(proto_blob)
     print("Success!")
     conn.close()
-
-def read_blob():
-    with open(BLOB_FILE, "rb") as blob:
-        response = title_detail_pb2.Response()
-        response.ParseFromString(blob.read())
-    if response.HasField("error"):
-        print(response.error)
-        sys.exit(1)
-    return response.success.titleDetailView
-
-def get_latest_chapter(title_info):
-    if len(title_info.lastChapterList) > 0:
-        chapter_list = title_info.lastChapterList
-    else:
-        chapter_list = title_info.firstChapterList
-    latest_chapter = chapter_list[-1]
-    latest_chapter_id = max([c.chapterId for c in chapter_list])
-    if latest_chapter.chapterId != latest_chapter_id:
-        print("Warning: last entry in chapter list is not the latest chapter")
-        print("Consider fixing the script to retrieve the chapter differently")
-    return latest_chapter
+    return Title(proto_blob)
 
 def save_metadata(data):
     # Check that data is correctly formed to not erase previous metadata with corrupt data
@@ -79,7 +82,9 @@ class Application(Gtk.Application):
         return False
 
     def notification_acknowledge(self, action, parameter):
-        save_metadata({"last_acknowledged_chapter": self.current_chapter.name})
+        with open(BLOB_FILE, "rb") as blob:
+            title_info = Title(blob.read())
+        save_metadata({"last_acknowledged_chapter": title_info.get_latest_chapter().name})
         print("Dismissed!")
         self.release()
 
@@ -98,8 +103,8 @@ class Application(Gtk.Application):
             os.mkdir(DATA_DIR)
         # If cached response blob is not found, fetch from the server then display latest chapter and exit
         if not os.path.isfile(BLOB_FILE) or not os.path.isfile(META_FILE):
-            fetch_and_save_title_info()
-            latest_chapter = get_latest_chapter(read_blob())
+            title_info = fetch_and_save_title_info()
+            latest_chapter = title_info.get_latest_chapter()
             save_metadata({"last_acknowledged_chapter": latest_chapter.name})
             
             self.show_notification("latest-chapter",
@@ -107,18 +112,19 @@ class Application(Gtk.Application):
                 f"Latest chapter: {latest_chapter.subTitle}\nReleased on {datetime.fromtimestamp(latest_chapter.startTimeStamp)}")
             return
 
-        title_info = read_blob()
-        current_chapter = get_latest_chapter(title_info)
+        with open(BLOB_FILE, "rb") as blob:
+            title_info = Title(blob.read())
+        current_chapter = title_info.get_latest_chapter()
         self.current_chapter = current_chapter
         date_current_chapter = datetime.fromtimestamp(current_chapter.startTimeStamp)
         print(f"Current last chapter: {current_chapter.name} ({date_current_chapter.date()})")
-        date_next_release = datetime.fromtimestamp(title_info.nextTimestamp)
+        date_next_release = title_info.get_next_release_date()
         print(f"Next chapter releases on {date_next_release}")
 
         if datetime.now() > date_next_release:
             print(f"Last release date was {date_next_release}, a new chapter should be released by now...")
-            fetch_and_save_title_info()
-            current_chapter = get_latest_chapter(read_blob())
+            title_info = fetch_and_save_title_info()
+            current_chapter = title_info.get_latest_chapter()
 
         with open(META_FILE, "r") as file:
             metadata = json.load(file)
